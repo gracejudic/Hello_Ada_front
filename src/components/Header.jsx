60b5f023949435b39d73b49433a7da87d94c281b
CharlotteBCK <charlotte44300@gmail.com>
2025-04-10 15:48:18 +0200

    modif lien assets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "C:/Users/charl/Documents/ADA/Projet_Collectif/Hello_Ada/Hello_Ada_Front/Hello_Ada_front/assets/hello ada.jpg";
+// import logo from "C:/Users/charl/Documents/ADA/Projet_Collectif/Hello_Ada/Hello_Ada_Front/Hello_Ada_front/assets/hello ada.jpg";
 
 function Header() {
   return (
     <header className="bg-rose-100 mt-1 flex items-center p-4">
       <img
-        src={logo}
+        src= "https://raw.githubusercontent.com/gracejudic/Hello_Ada_front/refs/heads/header/assets/hello%20ada.jpg"
         alt="Logo"
         className="h-20 w-auto ml-4"
       />
