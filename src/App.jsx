5dd6fa115207b23a8a4cefd0039a50c23a6d0124
CharlotteBCK <charlotte44300@gmail.com>
2025-04-23 17:04:15 +0200

    ajout de la page d'accueil

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 
 import './App.css';
 import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 function App() {
+
 
   return (
     <div>
       <Header/>
+      <HomePage/>
     </div>
   );
 
 
 }
 
-export default Header;
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,19 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="bg-rose-100 mt-1 p-4 my-20">
+    <header className="bg-rose-100 mt-1 p-8 ">
       <div className="max-w-1xl mx-auto flex flex-col md:flex-row items-center justify-between">
-        <img
-          src="file:///C:/Users/charl/Downloads/Hello,%20Ada%20!.svg"
+        <h1>Hello Ada </h1>
+        {/* <img
+          src="/assets/Hello, Ada !.png"
           alt="Logo"
           className="h-20 w-auto mb-4 md:mb-0"
-        />
+        /> */}
         <div className="flex gap-x-4 ml-auto">
-          <button className="py-2 px-6 bg-rose-500 hover:bg-rose-600 text-white font-bold rounded">
+          <button className="py-2 px-6 bg-rose-600 hover:bg-rose-600 text-white font-bold rounded">
             Créer un post
           </button>
-          <button className="py-2 px-6 bg-rose-500 hover:bg-rose-600 text-white font-bold rounded">
+          <button className="py-2 px-6 bg-rose-600 hover:bg-rose-600 text-white font-bold rounded">
           Connexion
           </button>
         </div>
