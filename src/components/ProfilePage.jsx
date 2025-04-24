9469c0a8c99a84cff4489b1989a41c70e6c6867b
Molid <delmolid@hotmail.fr>
2025-04-24 16:52:31 +0200

    creation de la connexion entre page d'accueil et page de profil

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import PostsDetails from "./PostsDetails";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 import UserDetails from "./UserDetails";
 import EditUser from "./EditUser";
 
 
 const ProfilePage = () => {
     return (
-        <Router>
             <div>
                 <Routes>
-                    <Route path="/users/:id" element={ 
+                    <Route path="/" element={ 
                         <div><div><UserDetails/></div>
                     <div><PostsDetails/></div><div/>
                     </div>
@@ -18,7 +17,6 @@ const ProfilePage = () => {
                     <Route path="/users/:id/updateProfile" element={<EditUser />} />
                 </Routes>
             </div>
-        </Router>
     );
 };
   
